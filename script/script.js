0cbd8c4b276c8c7319efc26377c664851726d510
vanezapata <vanezapata.0102@gmail.com>
2023-03-15 16:25:41 -0500

    modificar html y javascript

---
+++ script/script.js
@@ -0,0 +1,42 @@
+let nombre,correo,mensaje;
+let formulario = document.getElementById('form') 
+
+Formulario.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    leerDatos()
+})
+
+function leerDatos(){
+    nombre = document.getElementById('nombre').value
+    correo = document.getElementById('correo').value
+    mensaje = document.getElementById('textarea').value
+    validarData(nombre,correo,mensaje)
+    guardarlocalstorage(nombre,correo,mensaje)
+}
+
+function validarData(nombre,correo,mensaje) {
+    if(nombre.length==0 || correo.length==0 || mensaje.length==0){
+
+        Swal.fire({
+            title: 'error!',
+            text: 'do you want to continue',
+            icon:'error',
+            confirmbuttontext: 'cool',
+            iconcolor: 'purple'
+        })
+    }
+}
+
+function guardarlocalstorage(nombre,correo,mensaje){
+    localStorage.setItem('nombre',nombre)
+    localStorage.setItem('correo',correo)
+    localStorage.setItem('mensaje',mensaje)
+    listardata()
+}
+
+function listardata(){
+    let nombreUsu =  localStorage.getItem('nombre')
+    let correoUsu = localStorage.getItem('correo')
+    let mensajeUsu = localStorage.getItem('mensaje')
+
+}
